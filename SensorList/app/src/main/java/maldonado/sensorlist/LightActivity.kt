c04e811299c1ca985917105b2a96e365d34b7708
Percy <maldonadoq.percy@gmail.com>
2019-08-29 10:04:46 -0500

    All activity that I'm creating

---
+++ SensorList/app/src/main/java/maldonado/sensorlist/LightActivity.kt
@@ -0,0 +1,12 @@
+package maldonado.sensorlist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LightActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_light)
+    }
+}
