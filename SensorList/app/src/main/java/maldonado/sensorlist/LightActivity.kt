e17a616450947aed70da87791ddacf82af9e81eb
Percy <maldonadoq.percy@gmail.com>
2019-08-29 10:06:18 -0500

    Light Activity Class and Design

--- SensorList/app/src/main/java/maldonado/sensorlist/LightActivity.kt
+++ SensorList/app/src/main/java/maldonado/sensorlist/LightActivity.kt
@@ -1,12 +1,40 @@
 package maldonado.sensorlist
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_light.*
 
-class LightActivity : AppCompatActivity() {
+class LightActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var textView: TextView
+    private lateinit var sensorManager: SensorManager
+    private lateinit var sensor: Sensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_light)
+        setSupportActionBar(light_toolbar)
+
+        textView = findViewById(R.id.light_text)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        sensor = event!!.sensor
+        textView.text = "Light: ${event.values[0]}"
     }
 }
